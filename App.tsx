f9885d43b6f0f0a8070caf031e5e944d7f8df35f
김영선 <withbbang@gmail.com>
2023-10-25 00:12:27 +0900

    Chore: Build Error로 인한 프로젝트 재생성

---
+++ App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import RockStack from './screens/RockStack';
+
+function App() {
+  return (
+    <NavigationContainer>
+      <RockStack />
+    </NavigationContainer>
+  );
+}
+
+export default App;
