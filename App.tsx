c2e1131826139ab18ee09d3b017e617fb62308a7
김영선 <withbbang@gmail.com>
2023-10-26 00:15:56 +0900

    Test: Context Test

--- App.tsx
+++ App.tsx
@@ -8,11 +8,14 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RockStack from './screens/RockStack';
+import LogContext from './contexts/LogContext';
 
 function App() {
   return (
     <NavigationContainer>
-      <RockStack />
+      <LogContext.Provider value="안녕하세용용">
+        <RockStack />
+      </LogContext.Provider>
     </NavigationContainer>
   );
 }
---
+++ contexts/LogContext.js
@@ -0,0 +1,11 @@
+import {createContext} from 'react';
+
+/**
+ * 전역 상태 관리를 위한 첫번째 방법: context
+ * @return {Object} LogContext.Provider, LogContext.Consumer 컴포넌트 반환
+ * Provider는 Context안에 있는 값을 사용할 컴포넌트들을 감싸주는 용도로 사용
+ * 여기서는 App 컴포넌트에서 RockStack컴포넌트를 감싸기로 함
+ */
+const LogContext = createContext('안녕하세유유');
+
+export default LogContext;
