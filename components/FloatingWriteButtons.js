631907ee2cc98f75434b0eff97668dca37aa6f8b
김영선 <withbbang@gmail.com>
2023-10-26 00:56:18 +0900

    Feat: floating button component를 추가하여 write 페이지로 이동하기 기능 구현

---
+++ components/FloatingWriteButtons.js
@@ -0,0 +1,63 @@
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {Platform, View, StyleSheet, Pressable} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+function FloatingWriteButton() {
+  const navigation = useNavigation();
+
+  const onPress = () => {
+    navigation.navigate('write');
+  };
+
+  return (
+    <View style={styles.wrapper}>
+      {/* Touchable* 컴포넌트와 성격 비슷하지만 기능이 많음 */}
+      <Pressable
+        style={({pressed}) => [
+          styles.button,
+          Platform.OS === 'ios' && {
+            opacity: pressed ? 0.6 : 1,
+          },
+        ]}
+        android_ripple={{color: 'white'}}
+        onPress={onPress}>
+        <Icon name="add" size={24} style={styles.icon} />
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    // IOS 전용 그림자 설정
+    shadowColor: '#4d4d4d',
+    shadowOffset: {width: 0, height: 4},
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    // Android 전용 그림자 설정
+    elevation: 5,
+    // Android에서 물결 효과가 영역 밖으로 나가지 않도록 설정
+    // IOS에서는 overflow가 hidden일 경우 그림자가 보여지지 않음
+    overflow: Platform.select({android: 'hidden'}),
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    backgroundColor: '#009688',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    color: 'white',
+  },
+});
+
+export default FloatingWriteButton;
