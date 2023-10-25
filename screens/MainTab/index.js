19e3ab78b329f988a65b34049223f84427244b1b
김영선 <withbbang@gmail.com>
2023-10-25 23:55:40 +0900

    Style: 하단 탭 스타일링

--- screens/MainTab/index.js
+++ screens/MainTab/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import FeedsScreen from '../FeedsScreen';
 import CalendarScreen from '../CalendarScreen';
 import SearchScreen from '../SearchScreen';
@@ -8,10 +9,38 @@ const Tab = createBottomTabNavigator();
 
 function MainTab() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Feeds" component={FeedsScreen} />
-      <Tab.Screen name="Calendar" component={CalendarScreen} />
-      <Tab.Screen name="Search" component={SearchScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: '#009688', // 하단 탭 액티브 색깔
+        tabBarShowLabel: false, // 하단 탭 텍스트 보여주기 유무
+      }}>
+      <Tab.Screen
+        name="Feeds"
+        component={FeedsScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="view-stream" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Calendar"
+        component={CalendarScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="event" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="search" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
