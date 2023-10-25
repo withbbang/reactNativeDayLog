5405b7d14c8eb50d918a435864bd49d2143d805b
김영선 <withbbang@gmail.com>
2023-10-26 00:40:16 +0900

    Test: Context 테스트 종료로 인한 테스트 코드 원복
    - Feeds, Calendar

--- screens/FeedsScreen/index.js
+++ screens/FeedsScreen/index.js
@@ -1,27 +1,11 @@
-import React, {useContext} from 'react';
-import {StyleSheet, View, Text, TextInput} from 'react-native';
-import LogContext from '../../contexts/LogContext';
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
 
 function FeedsScreen() {
-  const {text, setText} = useContext(LogContext);
-
-  return (
-    <View style={styles.block}>
-      <TextInput
-        value={text}
-        onChangeText={setText}
-        placeholder={'텍스트를 입력하세용'}
-        style={styles.input}
-      />
-    </View>
-  );
+  return <View style={styles.block} />;
 }
 
 const styles = StyleSheet.create({
-  input: {
-    padding: 16,
-    backgroundColor: 'white',
-  },
   block: {},
 });
 
