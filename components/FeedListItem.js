39d9f5c2c1e601fbb070ed629c04565120c08f99
김영선 <withbbang@gmail.com>
2023-11-02 01:13:21 +0900

    Feat: date-fns를 이용한 피드 생성 날짜 포매팅

--- components/FeedListItem.js
+++ components/FeedListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Platform, Pressable, StyleSheet, Text} from 'react-native';
+import {format, formatDistanceToNow} from 'date-fns';
+import {ko} from 'date-fns/locale';
 
 /**
  * 정규식을 이용한 모든 줄 바꿈 문자 제거
@@ -16,6 +18,30 @@ function truncate(text) {
   return replaced.slice(0, 100).concat('...');
 }
 
+/**
+ * 방금 전 / 3분 전 / 1시간 전 / 3일 전 / 날짜
+ * 상위로 피드를 표기해 주는 date-fns를 이용한 변환 함수
+ * @param {Date} date
+ * @returns
+ */
+function formatDate(date) {
+  const d = new Date(date);
+  const now = Date.now();
+  const diff = (now - d.getTime()) / 1000;
+
+  if (diff < 60 * 1) {
+    return '방금 전';
+  }
+
+  if (diff < 60 * 60 * 24 * 3) {
+    // addSuffix: 포매팅 문자열 뒤에 '전' 혹은 '후' 접미사를 붙이는 옵션
+    return formatDistanceToNow(d, {addSuffix: true, locale: ko});
+  }
+
+  // PPP: 날짜, EEE: 요일, p: 시간
+  return format(d, 'PPP EEE p', {locale: ko});
+}
+
 function FeedListItem({log}) {
   const {title, body, date} = log;
 
@@ -26,7 +52,7 @@ function FeedListItem({log}) {
         Platform.OS === 'ios' && pressed && {backgroundColor: '#efefef'},
       ]}
       android_ripple={{color: '#ededed'}}>
-      <Text style={styles.date}>{new Date(date).toLocaleString()}</Text>
+      <Text style={styles.date}>{formatDate(date)}</Text>
       <Text style={styles.title}>{title}</Text>
       <Text style={styles.body}>{truncate(body)}</Text>
     </Pressable>
